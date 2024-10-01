5f2f8380b9bb1303b8efca95583c65c83125b661
Kimberly <kowens4@elon.edu>
2024-09-30 20:34:56 -0400

    sync

--- lib/booktracking-stack.ts
+++ lib/booktracking-stack.ts
@@ -1,14 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as cdk from 'aws-cdk-lib';
 import * as cognito from 'aws-cdk-lib/aws-cognito';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as amplify from 'aws-cdk-lib/aws-amplify';
+// import * as amplify from 'aws-cdk-lib/aws-amplify';
 import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
-import * as logs from 'aws-cdk-lib/aws-logs';
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BooktrackingStack extends cdk.Stack {
@@ -21,6 +17,11 @@ const userPool = new cognito.UserPool(this, 'UserPool', {
   signInAliases: {email: true},
   userPoolName: 'BookTrackingAppUserPool',
 });
+
+//Dynamodb Table for User Data (books read, reccomendations)
+const userDataTable = new dynamodb.Table(this, 'UserDataTable', {
+  partitionKey: { name: }
+})
 
 //Lambda Function for Adding a Book
 const addBookFunction = new lambda.Function(this, 'AddBookFunction', {
